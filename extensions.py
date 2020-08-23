8378ece610f4145fb58c432230649fcd8f3dfe51
alexBoldea <38548951+alexBoldea@users.noreply.github.com>
2020-08-24 00:42:16 +0300

    short script to check file extensions
    I've written this short script over the weekend to go through the list of filenames in .xlsx format generated by the main app and return a text file listing all different file extensions. The purpose is that when I will eventually get around to comparing and removing duplicates, I want to check only audio files and exclude other files (e.g. "front.jpg", an image of the front cover of an album, found hundreds if not thousands of times with the same name but depicting a different image)

---
+++ extensions.py
@@ -0,0 +1,43 @@
+#! python3
+
+import openpyxl
+import os
+import re
+
+songNameRegex = re.compile(r'.\w+$')                                            # regex to find extension goes here
+active = re.compile(r'.\S+')                                                    # regex to compare extension to the file contents (to get unique extensions)
+
+os.chdir('c:\\users\\alex_\\Desktop')
+path = "c:\\users\\alex_\\Desktop\\SongList.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active                                                       # selected the active sheet from the excel file
+
+inputFile = open('C:\\Users\\alex_\\Desktop\\extensions.txt', 'w')
+init = songNameRegex.search(sheet_obj.cell(row = 2, column = 1).value)
+ext = init.group()
+inputFile.write(' ' + ext + ' ')
+inputFile.close()                                                               # wrote the first extension in the text file
+
+
+for i in range (3, 36369):
+    mo = songNameRegex.search(sheet_obj.cell(row = i, column = 1).value)
+    inputFile = open('C:\\Users\\alex_\\Desktop\\extensions.txt', 'a')
+    inputFile.write(mo.group() + ' ')    
+    inputFile.close()                                                           # wrote all extensions in the file
+
+inputFile = open('C:\\Users\\alex_\\Desktop\\extensions.txt')
+boo = active.findall(inputFile.read())
+#print(boo)
+
+boo = list(dict.fromkeys(boo))                                                  # remove duplicates
+#print(boo)
+inputFile.close()
+
+inputFile = open('C:\\Users\\alex_\\Desktop\\extensions.txt', 'w')
+inputFile.write('Extensions: \n')
+inputFile.close()
+for i in range (len(boo)):
+    inputFile = open('C:\\Users\\alex_\\Desktop\\extensions.txt', 'a')
+    inputFile.write(boo[i] + '\n')    
+    inputFile.close()
+                                                                                # output is a list with all different file extensions
